29ac97787b8b75382a128ed89f6f05c166a81abe
goga <goga@goga-V5-171.(none)>
2013-03-31 23:29:50 +0400

    update graphics

--- Test/Data/base.glsl
+++ Test/Data/base.glsl
@@ -28,8 +28,10 @@ const float shininess = 1.0;
 
 void main(void)
 {
+  vec4 vtmp;
    posCamera = g_vEye;
-   posLight = g_mView * vec4(g_vLight.x, g_vLight.y, g_vLight.z, 0);
+   vtmp = g_mView * vec4(g_vLight.x, g_vLight.y, g_vLight.z, 0);
+   posLight = vtmp.xyz;
    //posLight = vec3(g_vLight.x, g_vLight.y, g_vLight.z);
    rngLight = g_vLight.w;
 
@@ -77,11 +79,12 @@ void main( void )
  vec4 color  = vec4(1, 1, 1, 1);
  //vec4 color1 = ambient;
  float alpha = color.a;
- 
+ float inten = intensity;
+
  vec3  v_lp = posLight - Position.xyz;
  float rangle = rngLight;
- intensity *= 30.0 / length(v_lp);
- color *= intensity;
+ inten *= 30.0 / length(v_lp);
+ color *= inten;
  color.a = 1.0;
  gl_FragColor = color;
  //gl_FragColor = vec4(1, 0, 0, 1);
