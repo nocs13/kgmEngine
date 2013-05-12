0558db88fbbd8e6ee057e13914a034271668e580
goga <goga@goga-V5-171.(none)>
2013-05-12 10:01:47 +0400

    add ai for player

---
+++ Test/AI/AITaoRen.h
@@ -0,0 +1,21 @@
+#ifndef AITAOREN_H
+#define AITAOREN_H
+
+#include "../../kgmGame/kgmIGame.h"
+#include "../../kgmGame/kgmGameLogic.h"
+
+class AITaoRen: public kgmGameLogic::AI
+{
+public:
+    void update(kgmActor* a, u32 mls)
+    {
+
+    }
+
+    void input(kgmActor* a, u32 btn, int state)
+    {
+
+    }
+};
+
+#endif // AITAOREN_H
