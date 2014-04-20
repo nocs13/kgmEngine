0920c7a957426c1fd1dbfa39579355e7a07a0687
nocs13 <nocs13@gmail.com>
2014-04-20 14:54:52 +0300

    ...

---
+++ Tools/Editor/kIcon.h
@@ -0,0 +1,14 @@
+#ifndef KICON_H
+#define KICON_H
+
+#include "../../kgmGraphics/kgmMesh.h"
+#include "../../kgmGraphics/kgmMaterial.h"
+
+class kIcon : public kgmMesh
+{
+  kgmMaterial* mrl;
+public:
+  kIcon();
+};
+
+#endif // KICON_H
