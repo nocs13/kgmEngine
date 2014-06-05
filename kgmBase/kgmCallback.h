7194f8032d1f2eea28108f4226f5b85c551bd8e7
Nocs13 <nocs13@gmail.com>
2014-06-05 11:26:43 +0400

    add callback class

---
+++ kgmBase/kgmCallback.h
@@ -0,0 +1,52 @@
+#ifndef KGMCALLBACK_H
+#define KGMCALLBACK_H
+
+class kgmObject;
+
+template <class F> class kgmCallback;
+
+template <class F, class... Args>
+class kgmCallback<F(Args...)>
+{
+public:
+  typedef F (*Function)(Args...);
+
+private:
+  kgmObject* object;
+  Function   function;
+
+public:
+  kgmCallback()
+  {
+    object = null;
+    function = null;
+  }
+
+  kgmCallback(kgmObject* obj = null, Function fn = null)
+  {
+    object = obj;
+    function = fn;
+  }
+
+  F operator()(Args... args)
+  {
+    return function(args...);
+  }
+
+  bool hasObject()
+  {
+    return (object != null);
+  }
+
+  bool hasFunction()
+  {
+    return (function != null);
+  }
+
+  kgmObject* getObject()
+  {
+    return object;
+  }
+};
+
+#endif // KGMCALLBACK_H
