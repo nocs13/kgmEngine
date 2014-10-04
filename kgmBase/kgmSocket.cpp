e739c1ffe2cb812d18599ae5282b4e9f4c44ba21
nocs13 <nocs13@gmail.com>
2014-10-05 00:07:57 +0400

    ...

---
+++ kgmBase/kgmSocket.cpp
@@ -0,0 +1,24 @@
+#include "kgmSocket.h"
+
+#include <unistd.h>
+
+KGMOBJECT_IMPLEMENT(kgmSocket, kgmObject);
+
+kgmSocket::kgmSocket(s32 proto, s32 type)
+{
+  protocol = proto;
+  domain = type;
+
+  handle = socket(type, proto, 0);
+}
+
+kgmSocket::~kgmSocket()
+{
+  clear();
+}
+
+void kgmSocket::clear()
+{
+  if(handle > 0)
+    close(handle);
+}
---
+++ kgmBase/kgmSocket.h
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef KGMSOCKET_H
+#define KGMSOCKET_H
+
+#include "kgmObject.h"
+#include "kgmString.h"
+
+#include <sys/socket.h>
+
+class kgmSocket : public kgmObject
+{
+  KGM_OBJECT(kgmSocket);
+
+public:
+  enum
+  {
+    ProtoTcp = SOCK_STREAM,
+    ProtoUdp = SOCK_DGRAM,
+  };
+
+  enum
+  {
+    InetIp4   = AF_INET,
+    InetIp6   = AF_INET6,
+    InetLocal = AF_LOCAL
+  };
+
+private:
+  s32 handle;
+  s32 domain;
+  s32 protocol;
+
+public:
+  kgmSocket(s32 proto = ProtoTcp, s32 type = InetIp4);
+  ~kgmSocket();
+
+  void clear();
+  bool connect(kgmString ip, u16 port);
+  bool listen(kgmString ip, u16 port);
+  u32  read(void* buf, u32 size);
+  u32  write(void* buf, u32 size);
+};
+
+#endif // KGMSOCKET_H
