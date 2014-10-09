4f370328041fc301bc1c4ddb343181011ba689c0
nocs13 <nocs13@gmail.com>
2014-10-09 23:24:16 +0400

    ...

--- kgmBase/kgmFileMemory.cpp
+++ kgmBase/kgmFileMemory.cpp
@@ -5,13 +5,64 @@ KGMOBJECT_IMPLEMENT(kgmFileMemory, kgmObject)
 kgmFileMemory::kgmFileMemory()
 {
   base   = null;
-  length = 0;
+  size   = 0;
   cursor = 0;
 }
 
 kgmFileMemory::kgmFileMemory(void *ptr, u32 len)
 {
   base   = (u8*)ptr;
-  length = len;
+  size   = len;
   cursor = 0;
+}
+
+u32 kgmFileMemory::length()
+{
+  return size;
+}
+
+u32 kgmFileMemory::position()
+{
+  return cursor;
+}
+
+u32 kgmFileMemory::seek(u32 pos)
+{
+  if(pos < size)
+    cursor = pos;
+
+  return cursor;
+}
+
+u32 kgmFileMemory::read(void* dst, u32 len)
+{
+  if(len < (size - cursor))
+  {
+    memcpy(dst, base + cursor, len);
+
+    cursor += len;
+
+    return len;
+  }
+
+  return 0;
+}
+
+u32 kgmFileMemory::write(void* src, u32 len)
+{
+  if(len < (size - cursor))
+  {
+    memcpy(base + cursor, src, len);
+
+    cursor += len;
+
+    return len;
+  }
+
+  return 0;
+}
+
+bool kgmFileMemory::eof()
+{
+  return (cursor >= size);
 }
--- kgmBase/kgmFileMemory.h
+++ kgmBase/kgmFileMemory.h
@@ -8,17 +8,20 @@ class kgmFileMemory : public kgmObject
   KGM_OBJECT(kgmFileMemory);
 
   u8*  base;
+  u32  size;
   u32  cursor;
-  u32  length;
 
 public:
   kgmFileMemory();
   kgmFileMemory(void *ptr, u32 len);
 
-  bool eof()
-  {
-    return (cursor >= length);
-  }
+  u32 length();
+  u32 position();
+  u32 seek(u32);
+  u32 read(void* dst, u32 len);
+  u32 write(void* src, u32 len);
+
+  bool eof();
 };
 
 #endif // KGMFILEMEMORY_H
