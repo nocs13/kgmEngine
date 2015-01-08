b232c60d79910ec9d9e9ee02a5e4d38235be1f30
Nocs13 <nocs13@gmail.com>
2015-01-08 23:14:06 +0400

    ...

--- kgmGame/objects/kgmLnCamera.cpp
+++ kgmGame/objects/kgmLnCamera.cpp
@@ -1,5 +1,7 @@
 #include "kgmLnCamera.h"
 #include "../kgmIGame.h"
+#include "../kgmILogic.h"
+#include "../kgmActor.h"
 #include "../../kgmGraphics/kgmIGraphics.h"
 
 KGMOBJECT_IMPLEMENT(kgmLnCamera, kgmSensor);
@@ -8,6 +10,13 @@ kgmLnCamera::kgmLnCamera(kgmIGame* g)
 :kgmSensor(g)
 {
   m_camera = null;
+  m_linked = null;
+}
+
+kgmLnCamera::~kgmLnCamera()
+{
+  if(m_linked)
+    m_linked->release();
 }
 
 void kgmLnCamera::init()
@@ -20,4 +29,25 @@ void kgmLnCamera::update(u32 ms)
 {
   if(m_camera == null)
     return;
+}
+
+bool kgmLnCamera::linkto(kgmString name)
+{
+  if(!game() || !game()->getLogic())
+    return false;
+
+  kgmActor *a = (kgmActor*)game()->getLogic()->getObjectById(name);
+
+  if(!a)
+    return false;
+
+  if(m_linked)
+    m_linked->release();
+
+  a->increment();
+
+  m_linked = a;
+  m_lname = name;
+
+  return true;
 }
--- kgmGame/objects/kgmLnCamera.h
+++ kgmGame/objects/kgmLnCamera.h
@@ -3,17 +3,24 @@
 
 #include "../kgmSensor.h"
 
+class kgmActor;
+
 class kgmLnCamera : public kgmSensor
 {
   KGM_GO_OBJECT(kgmLnCamera);
 
   kgmCamera *m_camera;
+  kgmActor  *m_linked;
+  kgmString  m_lname;
 
 public:
   kgmLnCamera(kgmIGame *game = null);
+  ~kgmLnCamera();
 
   void init();
   void update(u32 ms);
+
+  bool linkto(kgmString name);
 };
 
 #endif // KGMLNCAMERA_H
