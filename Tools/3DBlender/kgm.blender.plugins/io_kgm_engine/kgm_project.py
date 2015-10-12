415f8d20b409194f20165a0d9c3ae8aee1e6c560
nostradamus <nostradamus@gmail.com>
2015-10-12 18:24:47 +0400

    ...

--- Tools/3DBlender/kgm.blender.plugins/io_kgm_engine/kgm_project.py
+++ Tools/3DBlender/kgm.blender.plugins/io_kgm_engine/kgm_project.py
@@ -1,24 +1,45 @@
 # -*- coding: utf-8 -*-
+import os
 #import bpy
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
+import subprocess
 
 Units   = []
 Actors  = []
 Sensors = []
 
+Directory  = None
+Executable = None
+
+Process = None
+
 def parse(path):
-  tree = None
+  xmldoc = None
 
   try:
-    tree = ET.parse(path)
+    xmldoc = minidom.parse(path)
   except Exception as e:
     print('kgm project ' + path + ' cannot load as xml file. error: ' + e)
     return
 
-  root = tree.getroot()
+  Directory = os.path.dirname(path)
 
-  for elem in root.iter(None):
-    print('kgm project element: ' + elem.attrib)
+  items = xmldoc.getElementsByTagName('Executable');
 
-def run(cmd):
+  if len(items) > 0:
+    Executable = items[0].attributes['value'].value
+    print('kgm execuable is: ' + Executable);
+
+  items = xmldoc.getElementsByTagName('kgmActor');
+
+  if len(items) > 0:
+    for item in items:
+      Actors.append(item.attributes['id'].value)
+      print('kgm actor: ' + item.attributes['id'].value);
+
+def run(map):
+  if Executable is not None and Directory is not None:
+    cmd = Directory + '/' + Executable + ' map ' + map;
+    ls_output=subprocess.Popen([cmd], stdout=subprocess.PIPE)
   pass
