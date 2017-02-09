9ebad0ba0a531acc7ae9fcf96fa36adc46980521
nocs13 <nocs13@gmail.com>
2017-02-09 14:32:15 +0400

    adding BTree and Hashed Array.

---
+++ kgmBase/kgmBTree.h
@@ -0,0 +1,10 @@
+#ifndef KGMBTREE_H
+#define KGMBTREE_H
+
+template <class T>
+class kgmBTree
+{
+
+};
+
+#endif // KGMBTREE_H
---
+++ kgmBase/kgmHArray.h
@@ -0,0 +1,16 @@
+#ifndef KGMHARRAY_H
+#define KGMHARRAY_H
+
+#define KGM_HARRAY_DEFAULT_SIZE 64
+
+template <class T>
+class kgmHArray
+{
+public:
+  kgmHArray(u32 size = KGM_HARRAY_DEFAULT_SIZE)
+  {
+
+  }
+};
+
+#endif // KGMHARRAY_H
