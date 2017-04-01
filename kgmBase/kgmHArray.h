16c93680c3d9fb8035f67cd802a90063987a9dcd
nocs13 <nocs13@gmail.com>
2017-04-01 12:12:32 +0400

    ...

--- kgmBase/kgmHArray.h
+++ kgmBase/kgmHArray.h
@@ -6,10 +6,21 @@
 template <class T>
 class kgmHArray
 {
+  struct Node {
+    void* key;
+    u32   key_len;
+    T     data;
+
+    Node *next;
+  };
+
+  Node *root;
+
 public:
   kgmHArray(u32 size = KGM_HARRAY_DEFAULT_SIZE)
   {
-
+    root = new Node[size];
+    memset(root, 0, size * sizeof(Node));
   }
 };
 
