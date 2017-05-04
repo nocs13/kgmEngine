ee7d44fbc3db47a2f45f1b443386f3137dcd51ad
nocs13 <nocs13@gmail.com>
2017-05-04 13:32:17 +0400

    ...

--- kgmBase/kgmSort.h
+++ kgmBase/kgmSort.h
@@ -21,11 +21,11 @@ public:
     len   = length;
     check = compare;
 
-    sort_simple(data, len);
+    sort(data, len);
   }
 
 protected:
-  void sort_simple(T* data, u32 len)
+  void sort(T* data, u32 len)
   {
     Compare compare;
 
@@ -54,4 +54,130 @@ protected:
   }
 };
 
+template <class T>
+class kgmSortMerge
+{
+  kgmSort::Compare check;
+
+public:
+  kgmSortMerge(T* data, u32 len, Compare compare = nullptr)
+  {
+    if (compare)
+      check = compare;
+
+    sort(data, 0, len - 1);
+  }
+
+  void merge(int arr[], int l, int m, int r)
+  {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+      L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+      R[j] = arr[m + 1+ j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2)
+    {
+      if (L[i] <= R[j])
+      {
+        arr[k] = L[i];
+        i++;
+      }
+      else
+      {
+        arr[k] = R[j];
+        j++;
+      }
+      k++;
+    }
+
+    while (i < n1)
+    {
+      arr[k] = L[i];
+      i++;
+      k++;
+    }
+
+    while (j < n2)
+    {
+      arr[k] = R[j];
+      j++;
+      k++;
+    }
+  }
+
+  void sort(int arr[], int l, int r)
+  {
+    if (l < r)
+    {
+      int m = l+(r-l)/2;
+
+      sort(arr, l, m);
+      sort(arr, m+1, r);
+
+      merge(arr, l, m, r);
+    }
+  }
+};
+
+template <class T>
+class kgmSortHeap
+{
+  kgmSort::Compare check;
+
+public:
+  kgmSortHeap(T* data, u32 len, Compare compare = nullptr)
+  {
+    if (compare)
+      check = compare;
+
+    sort(data, len);
+  }
+
+  void heapify(T arr[], u32 n, u32 i)
+  {
+    u32 largest = i;
+    u32 l = 2 * i + 1;
+    u32 r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+      largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+      largest = r;
+
+    if (largest != i)
+    {
+      T t = arr[i];
+      arr[i] = arr[largest];
+      arr[largest] = t;
+
+      heapify(arr, n, largest);
+    }
+  }
+
+  void sort(T arr[], u32 n)
+  {
+    for (u32 i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+
+    for (u32 i=n-1; i>=0; i--)
+    {
+      T t = arr[0];
+      arr[0] = arr[i];
+      arr[i] = t;
+
+      heapify(arr, i, 0);
+    }
+  }
+};
 #endif // KGMSORT_H
