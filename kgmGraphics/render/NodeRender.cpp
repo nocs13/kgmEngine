e34a761e9c96abe76dc710f0b78842676dbb5493
nocs13 <nocs13@gmail.com>
2018-05-17 18:16:11 +0400

    ...

---
+++ kgmGraphics/render/NodeRender.cpp
@@ -0,0 +1,18 @@
+/*
+ * NodeRender.cpp
+ *
+ *  Created on: May 17, 2018
+ *      Author: guram
+ */
+
+#include "NodeRender.h"
+
+NodeRender::NodeRender() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NodeRender::~NodeRender() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ kgmGraphics/render/NodeRender.h
@@ -0,0 +1,22 @@
+/*
+ * NodeRender.h
+ *
+ *  Created on: May 17, 2018
+ *      Author: guram
+ */
+
+#ifndef KGMGRAPHICS_RENDER_NODERENDER_H_
+#define KGMGRAPHICS_RENDER_NODERENDER_H_
+
+#include "../../kgmBase/kgmIGC.h"
+#include "../kgmGui.h"
+#include "BaseRender.h"
+
+class NodeRender: public BaseRender
+{
+public:
+	NodeRender();
+	virtual ~NodeRender();
+};
+
+#endif /* KGMGRAPHICS_RENDER_NODERENDER_H_ */
