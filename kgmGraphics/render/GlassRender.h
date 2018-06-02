ae1859514f9beb95b5bfd4d461eb567353056a70
nocs13 <nocs13@gmail.com>
2018-06-02 14:50:58 +0400

    ...

--- kgmGraphics/render/GlassRender.h
+++ kgmGraphics/render/GlassRender.h
@@ -2,11 +2,23 @@
 #include "../../kgmBase/kgmIGC.h"
 #include "../kgmGui.h"
 
+class INode;
+class Mesh;
+
+
 class GlassRender: public BaseRender
 {
+  struct Node
+  {
+    INode *node;
+    Mesh  *mesh;
+  };
+
  public:
   GlassRender(kgmGraphics* gr);
 
+  void clear();
   void render();
 
+  void add(INode*);
 };
