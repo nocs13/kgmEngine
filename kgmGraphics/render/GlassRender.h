de306541114eda61e30ef68d9a1961aa3aa858ac
nocs13 <nocs13@gmail.com>
2018-06-03 00:24:33 +0400

    ...

--- kgmGraphics/render/GlassRender.h
+++ kgmGraphics/render/GlassRender.h
@@ -8,17 +8,31 @@ class Mesh;
 
 class GlassRender: public BaseRender
 {
+  enum TypeReflect
+  {
+    TypeNone,
+    TypePlane,
+    TypeCube
+  };
+
   struct Node
   {
     INode *node;
     Mesh  *mesh;
+
+    void *reflect;
+
+    TypeReflect type;
   };
+
+  void *tex_refract;
 
  public:
   GlassRender(kgmGraphics* gr);
 
   void clear();
   void render();
+  void prepare();
 
   void add(INode*);
 };
