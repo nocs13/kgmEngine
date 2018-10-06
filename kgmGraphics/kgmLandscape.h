74c08a6f936ea48a25a2d783c91db41b84284ae6
Nocs13 <nocs13@gmail.com>
2018-10-06 15:37:06 +0400

    ...

--- kgmGraphics/kgmLandscape.h
+++ kgmGraphics/kgmLandscape.h
@@ -10,15 +10,46 @@ class kgmLandscape: public kgmObject
   KGM_OBJECT(kgmLandscape);
 
 private:
-  struct Heightmap
-  {
-    //kgmPicture* map;
-  };
+  f32 m_width;
+  f32 m_length;
+  f32 m_height;
+
+  kgmPicture *m_heighmap;
 
 protected:
   ~kgmLandscape();
 
 public:
   kgmLandscape();
+
+  f32 width() const
+  {
+    return m_width;
+  }
+
+  void width(f32 w)
+  {
+    m_width = w;
+  }
+
+  f32 height() const
+  {
+    return m_height;
+  }
+
+  void height(f32 h)
+  {
+    m_height = wh;
+  }
+
+  f32 length() const
+  {
+    return m_length;
+  }
+
+  void length(f32 l)
+  {
+    m_length = l;
+  }
 };
 
