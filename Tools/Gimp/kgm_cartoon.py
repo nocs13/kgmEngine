f0fbf7d755fafe3fd03025ad37cccc93e7ad9b28
nocs13 <nocs13@gmail.com>
2018-11-23 17:16:29 +0400

    ...

--- Tools/Gimp/kgm_cartoon.py
+++ Tools/Gimp/kgm_cartoon.py
@@ -2,6 +2,36 @@
 # -*- coding: utf-8 -*-
 from gimpfu import *
 
+class Colormap:
+  def __init__(self):
+    self.ranges = []
+    self.ranges[0] = [0,   30]
+    self.ranges[1] = [31,  60]
+    self.ranges[2] = [61,  90]
+    self.ranges[3] = [91,  120]
+    self.ranges[4] = [121, 150]
+    self.ranges[5] = [151, 180]
+    self.ranges[6] = [181, 210]
+    self.ranges[7] = [211, 249]
+    self.ranges[8] = [250, 255]
+
+    self.colors = []
+    self.colors[0] = [0, 0, 0]
+    self.colors[1] = [101, 69, 39]
+    self.colors[2] = [150, 140, 200]
+    self.colors[3] = [90, 167, 90]
+    self.colors[4] = [255, 105, 180]
+    self.colors[5] = [145, 90, 199]
+    self.colors[6] = [45, 200, 120]
+    self.colors[7] = [78, 145, 134]
+    self.colors[8] = [255, 255, 255]
+
+  def rgb(self, gray):
+    for i in range(0, len(self.ranges)):
+      if gray >= self.ranges[i][0] and gray <= self.ranges[i][1]:
+        return self.colors[i]
+    return [0, 0, 0]
+
 def cartoon(r, g, b):
   z = 255 / 3
   color = [r, g, b]
@@ -18,7 +48,15 @@ def to_gray(r, g, b):
   return int(gray)
 
 def to_rgb(gray):
-  pass
+  rgb = []
+
+  #if (gray < 1):
+  #  rgb = [0, 0, 0]
+  #else if gray > 254:
+  #  rgb = [255, 255, 255]
+
+  return rgb
+
 
 def kgm_cartoon(image, layer) :
   # Indicates that the process has started.
@@ -29,6 +67,8 @@ def kgm_cartoon(image, layer) :
 
   pc = 1.0 / (layer.width + 1)
 
+  colormap = Colormap()
+
   try:
     for x in range(layer.width):
       for y in range(layer.height):
@@ -38,7 +78,9 @@ def kgm_cartoon(image, layer) :
           #sum = pixel[0] + pixel[1] + pixel[2]
           #gray = int(sum/3)
           gray = to_gray(pixel[0], pixel[1], pixel[2])
-          newColor = (gray, gray, gray)
+          rgb = colormap.rgb(gray)
+          newColor = (rgb[0], rgb[1], rgb[2])
+          #newColor = (gray, gray, gray)
           #newColor = (0, gray, 0)
           #newColor = cartoon(pixel[0], pixel[1], pixel[2])
           #newColor = cartoon(pixel[0], pixel[1], pixel[2])
