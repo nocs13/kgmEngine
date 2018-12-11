505db13bfd21d63596e8b902bf4583b12570fbaa
Goga <Goga@Root-PC>
2018-12-11 17:01:41 +0400

    ...

---
+++ kgmGame/kgmGameTerrain.cpp
@@ -0,0 +1,20 @@
+/*
+ * kgmGameTerrain.cpp
+ *
+ *  Created on: Dec 9, 2018
+ *      Author: Goga
+ */
+
+#include "kgmGameTerrain.h"
+
+kgmGameTerrain::kgmGameTerrain()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+kgmGameTerrain::~kgmGameTerrain()
+{
+  // TODO Auto-generated destructor stub
+}
+
