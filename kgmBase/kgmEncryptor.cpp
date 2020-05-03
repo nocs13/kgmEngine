78938390564d6f0a9880879428f9c8fdc2937d84
Nocs nocs <nocs13@gmail.com>
2020-05-03 14:34:45 +0400

    ---

---
+++ kgmBase/kgmEncryptor.cpp
@@ -0,0 +1,6 @@
+#include "kgmEncryptor.h"
+
+kgmEncryptor::kgmEncryptor()
+{
+
+}
---
+++ kgmBase/kgmEncryptor.h
@@ -0,0 +1,14 @@
+#ifndef KGMENCRYPTOR_H
+#define KGMENCRYPTOR_H
+
+#include "kgmObject.h"
+
+class kgmEncryptor : public kgmObject
+{
+  KGM_OBJECT(kgmEncryptor);
+
+public:
+  kgmEncryptor();
+};
+
+#endif // KGMENCRYPTOR_H
