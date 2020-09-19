846e74bc2d36b906558c8802db0fc5f19121a7d8
Nocs nocs <nocs13@gmail.com>
2020-09-19 17:39:39 +0400

    ---

--- kgmBase/kgmMemory.cpp
+++ kgmBase/kgmMemory.cpp
@@ -4,6 +4,91 @@
 
 //#include "kgmLog.h"
 
+typedef u_int32_t uint32;
+
+void* kgm_alloc(size_t size);
+void* kgm_realloc(void *p, size_t size);
+void kgm_free(void* p);
+
+struct _k_Memory;
+
+struct _k_Memory
+{
+  void*  p;
+  uint32 s;
+  uint32 i;
+};
+
+#define _K_MEMORY_CHUNK 512
+
+_k_Memory* _memory = nullptr;
+
+unsigned int _len = 0;
+unsigned int _cur = -1;
+
+void _k_memory_init()
+{
+  if (_memory)
+    return;
+
+  _memory = (_k_Memory*) kgm_alloc(sizeof(_k_Memory) * _K_MEMORY_CHUNK);
+
+  _len = _K_MEMORY_CHUNK;
+  _cur = -1;
+}
+
+unsigned int _k_memory_alloc(size_t size)
+{
+  if (size < 1)
+    return -1;
+
+  if (_cur == _len)
+  {
+    _memory = (_k_Memory*) kgm_realloc(_memory, sizeof(_k_Memory) * (_K_MEMORY_CHUNK + _len));
+
+    _len += _K_MEMORY_CHUNK;
+
+    for (auto i = _cur; i < _len; i++)
+      _memory[i] = {nullptr, 0, (uint32) -1};
+  }
+
+  _k_Memory m;
+
+  m.p = kgm_alloc(size);
+  m.s = size;
+  m.i = _cur;
+
+  _memory[_cur] = m;
+
+  _cur ++;
+
+  while(_cur < _len)
+  {
+    if (_memory[_cur].i == -1)
+      break;
+
+    _cur ++;
+  }
+
+  return m.i;
+}
+
+void _k_memory_free(unsigned int i)
+{
+  if (i == -1 || i > _len)
+    return;
+
+  if (_memory[i].i == -1)
+    return;
+
+  if (_memory[i].p != nullptr)
+    kgm_free(_memory[i].p);
+
+  _cur = i;
+
+  _memory[i] = {nullptr, 0, (uint32) -1};
+}
+
 void* kgm_alloc(size_t size)
 {
   if (size < 1)
--- kgmGame/kgmGameDraw.h
+++ kgmGame/kgmGameDraw.h
@@ -2,10 +2,76 @@
 #define KGMGAMEDRAW_H
 
 #include "../kgmBase/kgmObject.h"
+#include "../kgmMath/kgmMath.h"
+
+class kgmMesh;
+class kgmMaterial;
 
 class kgmGameDraw : public kgmObject
 {
   KGM_OBJECT(kgmGameDraw);
+
+  enum Type
+  {
+    TypeNone,
+    TypeMesh,
+    TypeText,
+    TypeSprite,
+    TypeBillboard,
+    TypeParticles
+  };
+
+  struct Draw {
+
+  };
+
+  struct  DrawMesh: Draw
+  {
+    kgmMesh*     msh;
+    kgmMaterial* mtl;
+    mtx4         trn;
+
+    DrawMesh()
+    {
+
+    }
+  };
+
+  struct DrawText: Draw
+  {
+
+    DrawText()
+    {
+
+    }
+  };
+
+  struct DrawSprite: Draw
+  {
+
+    DrawSprite()
+    {
+
+    }
+  };
+
+  struct DrawParticles: Draw
+  {
+
+    DrawParticles()
+    {
+
+    }
+  };
+
+  struct DrawBillboard: Draw
+  {
+
+    DrawBillboard()
+    {
+
+    }
+  };
 
 public:
   kgmGameDraw();
