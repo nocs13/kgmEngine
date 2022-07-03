2717c1a09f3d7244d685810b6b13ba6dc077312d
G G <nocs13@gmail.com>
2022-07-03 17:11:18 +0400

    ---

--- kgmGraphics/kgmGuiContainer.cpp
+++ kgmGraphics/kgmGuiContainer.cpp
@@ -34,11 +34,35 @@ void kgmGuiContainer::update() {
 
   u32 itemHeight = 40;
   u32 row = 1;
+  u32 cols = 0;
 
   auto i = _cells.begin();
 
   while(!i.end()) {
+    if (cols >= (u32) CellSize_5) {
+      row ++;
+      cols = 0;
+    }
 
+    u32 x = row * itemHeight + cols * cellWidth;
+    u32 y = row * itemHeight;
+    u32 w = (*i).width;
+    u32 h = itemHeight;
+
+    if ((cols + w) > CellSize_5) {
+      w = CellSize_5 - cols;
+    }
+
+    if (w < 1)
+      continue;
+
+    if ((*i).gui) {
+      Rect rc(x, y, w * cellWidth, h);
+
+      (*i).gui->setRect(rc);
+    }
+
+    cols += w;
   }
 }
 
