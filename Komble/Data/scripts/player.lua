e410e9ffb0fea39f5c421b09e0afad25a5987c97
G G <nocs13@gmail.com>
2022-11-13 00:12:12 +0400

    ---

--- Komble/Data/scripts/player.lua
+++ Komble/Data/scripts/player.lua
@@ -13,3 +13,11 @@ end
 function Player:state(s)
   self.s = s
 end
+
+function Player:input()
+  if kgmKeyState(KEY_UP) == 1 then
+  elseif kgmKeyState(KEY_DOWN) == 1 then
+  elseif kgmKeyState(KEY_LEFT) == 1 then
+  elseif kgmKeyState(KEY_RIGHT) == 1 then
+  end
+end
